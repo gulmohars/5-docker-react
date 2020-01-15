5a89610a947b69d7db098f89f70a64c32eca1f3a
Gulmohar <gulmohar@sunil.kashyap@chimp.net>
2020-01-15 12:28:26 +0530

    added travis.yml file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Sir!</p>
+        <p>Hello Sir! I am running Production Ready Build</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
